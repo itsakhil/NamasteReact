41e86728f5cbe74bc5711e0de3e8def036da3aae
itsakhil <123akhilchaitanya@gmail.com>
2025-04-12 18:15:49 +0530

    IgnitingApp

--- App.js
+++ App.js
@@ -1,51 +1,29 @@
 ///const heading =React.createElement('h1',{},"Hello wORLD ");
 
-
-const nestedReact=React.createElement(
+import React from "react";
+import ReactDOM from "react-dom/client"
+const nestedReact = React.createElement(
     "div",
-    {id:"Parent"},
+    { id: "Parent" },
     [
-        React.createElement
-        (
-            "div",
-            {id:"child1"},
-            [
-                React.createElement
-                (
-                 "h1",
-                {id:"sib1"},
-                "Heading1"
-                ),
-                React.createElement
-                (
-                 "h2",
-                {id:"sib2"},
-                "Heading2"
-                )
-            ]
-        ),
-        React.createElement
-        (
-            "div",
-            {id:"child2"},
-            [
-                React.createElement
-                (
-                 "h1",
-                {id:"sib1"},
-                "Heading1"
-                ),
-                React.createElement
-                (
-                 "h2",
-                {id:"sib2"},
-                "Heading2"
-                )
-            ]
-        )
+      React.createElement(
+        "div",
+        { id: "child1", key: "child1" },
+        [
+          React.createElement("h1", { id: "sib1", key: "h1-1" }, "Akhil"),
+          React.createElement("h2", { id: "sib2", key: "h2-1" }, "Chaitanya")
+        ]
+      ),
+      React.createElement(
+        "div",
+        { id: "child2", key: "child2" },
+        [
+          React.createElement("h1", { id: "sib1", key: "h1-2" }, "Heading1"),
+          React.createElement("h2", { id: "sib2", key: "h2-2" }, "Heading2")
+        ]
+      )
     ]
-
-)
-
+  );
+  
 const root =ReactDOM.createRoot(document.getElementById("root"))
 root.render(nestedReact);
